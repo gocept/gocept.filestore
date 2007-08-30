21a67231bc2ed5aad2e67a80c1d94954ed179659
Christian Zagrodnick <cz@flyingcircus.io>
2007-08-30 11:27:23 +0000

    created package for filestore
    also added some tests

---
+++ src/gocept/filestore/tests.py
@@ -0,0 +1,14 @@
+# Copyright (c) 2007 gocept gmbh & co. kg
+# See also LICENSE.txt
+# $Id$
+
+import unittest
+
+from zope.testing import doctest
+
+def test_suite():
+    suite = unittest.TestSuite()
+    suite.addTest(doctest.DocFileSuite(
+        'README.txt',
+        optionflags=doctest.ELLIPSIS))
+    return suite
