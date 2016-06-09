8e69ab5734d4e2ba039bdf5e8aa344db909ade90
Steffen Allner <sa@gocept.com>
2016-06-09 10:02:35 +0200

    Migrate repository to Bitbucket.

--- setup.py
+++ setup.py
@@ -1,37 +1,33 @@
-# Copyright (c) 2007-2014 gocept gmbh & co. kg
-# See also LICENSE.txt
-# $Id$
-
 import os.path
 
 from setuptools import setup, find_packages
 
 
 setup(
-    name = 'gocept.filestore',
-    version = '0.4dev',
-    author = "Christian Zagrodnick",
-    author_email = "cz@gocept.com",
-    description = "Provides maildir like access to files",
-    long_description = open(os.path.join(os.path.dirname(__file__),
-                                         'src', 'gocept', 'filestore',
-                                         'README.txt')).read(),
-    license = "ZPL 2.1",
-    url='http://pypi.python.org/pypi/gocept.filestore',
+    name='gocept.filestore',
+    version='0.4dev',
+    author='gocept gmbh & co. kg',
+    author_email='mail@gocept.com',
+    url='https://bitbucket.org/gocept/gocept.filestore',
+    description="Provides maildir like access to files",
+    long_description=open(os.path.join(os.path.dirname(__file__),
+                                       'src', 'gocept', 'filestore',
+                                       'README.txt')).read(),
+    license="ZPL 2.1",
 
-    packages = find_packages('src'),
-    package_dir = {'': 'src'},
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
 
-    include_package_data = True,
-    zip_safe = False,
+    include_package_data=True,
+    zip_safe=False,
 
-    namespace_packages = ['gocept'],
-    install_requires = [
+    namespace_packages=['gocept'],
+    install_requires=[
         'setuptools',
         'zope.deferredimport',
         'zope.interface',
     ],
-    extras_require = {
+    extras_require={
         'test': ['zope.testing'],
     },
-    )
+)
