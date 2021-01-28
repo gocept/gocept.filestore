f967fb911a30f6745aaad235ea20728181963dea
Steffen Allner <sa@gocept.com>
2021-01-28 15:19:47 +0100

    Use pre-commit for linting.

--- src/gocept/filestore/__init__.py
+++ src/gocept/filestore/__init__.py
@@ -1,7 +1,7 @@
-# coding: UTF-8
 
 import zope.deferredimport
 
+
 zope.deferredimport.define(
     FileStore='gocept.filestore.filestore:FileStore'
-    )
+)
--- src/gocept/filestore/filestore.py
+++ src/gocept/filestore/filestore.py
@@ -1,4 +1,3 @@
-# coding: UTF-8
 
 import gocept.filestore.interfaces
 import os
